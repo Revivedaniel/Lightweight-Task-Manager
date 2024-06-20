2dc5b151ebe687411c4b360ccb26dd870ea77a06
Revivedaniel <mlg.danielstark@gmail.com>
2024-06-20 13:26:31 -0700

    Added Angular Material to project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,4 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Lightweight-Task-Manager';
 }
