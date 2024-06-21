c3e28748d2b29b92bb59bdaaf254bf5a52168f47
Revivedaniel <mlg.danielstark@gmail.com>
2024-06-21 14:03:47 -0700

    The dynamic header component now displays buttons based on inputs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { DynamicHeaderComponent, HeaderButton } from './shared/dynamic-header/dynamic-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DynamicHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   // TODO: Create a service to manage the tasks.
   // TODO: Make the app a PWA.
+  buttons: HeaderButton[] = [
+    {
+      functionalityType: 'Link',
+      contentType: 'Text',
+      content: 'Home',
+      link: '/home',
+    },
+    {
+      functionalityType: 'Link',
+      contentType: 'Icon',
+      content: 'add',
+      link: '/new',
+    },
+  ];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { DynamicHeaderComponent } from '../shared/dynamic-header/dynamic-header.component';
 import { TaskComponent } from '../components/task/task.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DynamicHeaderComponent, TaskComponent],
+  imports: [TaskComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
